3035f1891ef69e731844cbe36df467b95ba8c9ee
Eko Mardiatno <ekomardiatno@gmail.com>
2025-01-15 01:11:44 +0700

    reset visit count feature

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 import os
 import redis
 
@@ -12,16 +12,18 @@ debug_mode = True if flask_env == 'development' else False
 
 @app.route('/')
 def home():
-  return "Hello, Docker!"
-
-@app.route('/flask-env')
-def flask_env():
-  return f"Flask ENV: {flask_env}"
-
-@app.route('/visit-count')
-def visit_count():
   visit_count = r.incr('visit_count')
-  return f"This page has been visited {visit_count}"
+  if visit_count is None:
+    visit_count = 0
+  else:
+    visit_count = int(visit_count)
+
+  return render_template('index.html', visit_count=visit_count)
+
+@app.route('/reset')
+def reset():
+  r.set('visit_count', 0)
+  return redirect(url_for('home'))
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5000, debug=debug_mode)
